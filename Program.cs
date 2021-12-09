caca0c932a77da0aee7472eb07d4d95ce27e0beb
Solomia <solka.marsolka@gmail.com>
2021-12-09 12:19:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleAppBook
+{
+    class Book
+    {
+        private string title; // назва (заголовок) книги
+        private string author; // прізвище й ім'я автора
+        private double price; // вартість книги
+
+        public Book(string _title, string _author, double _price)
+        {
+            title = _title;
+            author = _author;
+
+            // корегування вартості книги
+            if (price < 0) price = 0.0;
+            else price = _price;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("Title - 01", "Author - 01", 122.25);
+        }
+    }
+}
